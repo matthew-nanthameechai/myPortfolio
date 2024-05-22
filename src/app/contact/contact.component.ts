9245e9124cb42e1e40acbab7748f0907ce354386
Matthew N <mattnanthameechai@gmail.com>
2024-05-22 16:13:52 +1200

    sends email

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm('service_vvoq4l8', 'template_s8elh1b', e.target as HTMLFormElement, {
+        publicKey: 'CMBHTXESjKjbeelL6',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
